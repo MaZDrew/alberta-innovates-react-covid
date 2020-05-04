c93adddceb854c617ecdaed58c442934d957b8c8
MaZDrew <morgan.zuk@ucalgary.ca>
2020-05-03 23:46:04 -0600

    Create React App Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './components/Dashboard'
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
+import {Switch, Route} from 'react-router-dom';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#ffac42',
+      main: '#f47b00',
+      dark: '#ba4c00',
+      contrastText: '#000',
+    },
+    secondary: {
+      light: '#a4a4a4',
+      main: '#757575',
+      dark: '#494949',
+      contrastText: '#fff',
+    },
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Switch>
+        <Route exact path='/' render={props => <Dashboard {...props}/>}/>
+      </Switch>
+    </ThemeProvider>
   );
 }
 
